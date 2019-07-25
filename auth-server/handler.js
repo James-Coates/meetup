dfa98cacc892836d044510590d9e6e888346e696
james <james.coates@me.com>
2019-07-25 18:49:34 +0100

    change meetup OAuth consumer

--- auth-server/handler.js
+++ auth-server/handler.js
@@ -4,18 +4,19 @@
 const axios = require('axios');
 
 // Meetup OAUTH details
-const consumerKey = 'hi1r286u66ertam55qqihmjp1b';
-const consumerSecret = 'mop6jupuvn7j6aaa6kpi60e4em';
-const redirectURI = 'https://james-coates.co.uk/';
+const consumerKey = 'n9bvuhvmod0qgbtuog9kadtfcj';
+const consumerSecret = '2m408k4cfjcn8e7r3vucrfog82';
+const redirectURI = 'https://james-coates.co.uk/meetup/';
 
 module.exports.getAccessToken = async (event) => {
 
   const MEETUP_OAUTH_URL = 'https://secure.meetup.com/oauth2/access'
-    + '?client_id=hi1r286u66ertam55qqihmjp1b'
+    + '?client_id=' + consumerKey // CONSUMER KEY
     + '&client_secret=' + consumerSecret
     + '&grant_type=authorization_code'
     + '&redirect_uri=' + redirectURI
     + '&code=' + event.pathParameters.code; // DYNAMIC AUTHORIZATION CODE
+    // + '&code=a5938f4322363dcc56712b53a1cef336'
 
   const info = await axios.post(MEETUP_OAUTH_URL);
 
@@ -26,7 +27,7 @@ module.exports.getAccessToken = async (event) => {
     },
     body: JSON.stringify({
       accessToken: info.data.access_token,
-      refreshToken: info.data.refresh_token
+      refreshToken: info.data.refresh_token,
     })
   }
 }
@@ -44,7 +45,7 @@ module.exports.refreshAccessToken = async (event) => {
   return {
     statusCode: 200,
     headers: {
-      'Access-Control-Allow-Origin': '*'
+      'Access-Control-Allow-Origin': 'http://127.0.0.1:5500'
     },
     body: JSON.stringify({
       accessToken: info.data.access_token,
@@ -57,10 +58,12 @@ module.exports.refreshAccessToken = async (event) => {
 
 
 
+// Redirect URI
+// https://secure.meetup.com/oauth2/authorize?client_id=n9bvuhvmod0qgbtuog9kadtfcj&response_type=code&redirect_uri=https://james-coates.co.uk/meetup/
+// https://secure.meetup.com/oauth2/authorize?client_id=n9bvuhvmod0qgbtuog9kadtfcj&response_type=code&redirect_uri=https://james-coates.co.uk/
 
-// https://secure.meetup.com/oauth2/authorize?client_id=hi1r286u66ertam55qqihmjp1b&response_type=code&redirect_uri=https://james-coates.co.uk/
-// https://secure.meetup.com/oauth2/authorize?client_id=hi1r286u66ertam55qqihmjp1b&response_type=code&redirect_uri=https://james-coates.co.uk/
-
+// https://secure.meetup.com/oauth2/access?client_id=hi1r286u66ertam55qqihmjp1b&client_secret=n4hh0j03k2ms98aj61r50mlkrm&grant_type=authorization_code&redirect_uri=https://james-coates.co.uk/meetup/&code=c362ff810374e710ecacdecc9413ad1c
+// c362ff810374e710ecacdecc9413ad1c
 // https://secure.meetup.com/oauth2/access/client_id=hi1r286u66ertam55qqihmjp1b&client_secret=mop6jupuvn7j6aaa6kpi60e4em&grant_type=refresh_token&refresh_token=8b183ab520a553de065fc611bb43d2c7
 
 // https://m49ckcqvs2.execute-api.eu-west-2.amazonaws.com/dev/api/token/ad5401cea1a6e704dcd0b522fff689c4
