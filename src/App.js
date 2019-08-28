fc69dcc727a29199e147d6cfe0fd1f4cf71f29b4
james <james.coates@me.com>
2019-08-28 17:55:46 +0100

    add graph

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
+import moment from 'moment';
 import EventList from './EventList'
 import CitySearch from './CitySearch'
 import NumberOfEvents from './NumberOfEvents';
 import Header from './Header';
-import { infoAlert, InfoAlert } from './Alert';
+import { InfoAlert } from './Alert';
 import { getEvents } from './api';
 import { Container } from 'react-bootstrap';
+import { ResponsiveContainer, ScatterChart, Scatter, XAxis, YAxis, CartesianGrid, Tooltip } from 'recharts';
 import './App.css';
 
 class App extends Component {
@@ -15,7 +17,7 @@ class App extends Component {
     events: [],
     city: {},
     alert: ''
-  }
+  };
 
   componentDidMount() {
     this._isMounted = true;
@@ -25,7 +27,7 @@ class App extends Component {
       this.setState({alert: ''});
     };
     this.updateEvents(undefined, undefined, 32);
-  }
+  };
 
   updateEvents = (lat, lon, page) => {
     getEvents(lat, lon, page).then(data => {
@@ -34,25 +36,58 @@ class App extends Component {
         this.setState({city, events});
       }
     });
-  }
+  };
+
+  getData = () => {
+    const next7Days = [];
+    const currentDate = moment();
+    // Loop 7 times for each day
+    for (let i = 0; i < 7; i++) {
+      currentDate.add(1, 'days');
+      const dateString = currentDate.format('YYYY-MM-DD');
+      const n = this.countEventsOnDate(dateString);
+      next7Days.push({date: dateString, number: n});
+    }
+    return next7Days;
+  };
+
+  countEventsOnDate = (date) => {
+    let n = 0; // Number of events
+    this.state.events.forEach((event) => {
+      if(event.local_date === date) n++;
+    });
+    return n;
+  };
 
   componentWillUnmount() {
     this._isMounted = false;
-  }
+  };
   
   render() {
     return (
       <div className="App">
         <Header />
         <Container className="main-container">
+          <h1>test2</h1>
           <CitySearch updateEvents={this.updateEvents}/>
           { this.state.alert ? <InfoAlert text={this.state.alert} /> : '' }
+          
+          <ResponsiveContainer height={400}>
+            <ScatterChart margin={{top: 20, right: 20, bottom: 20, left: 20}}>
+              <CartesianGrid />
+              <XAxis type="category" dataKey="date" />
+              <YAxis type="number" dataKey="number" allowDecimals={false} />
+              <Tooltip cursor={{ strokeDasharray: '3 3' }} />
+              <Scatter data={this.getData()} fill="#8884d8" />
+            </ScatterChart>
+          </ResponsiveContainer>
+
           <EventList events={this.state.events} />
           <NumberOfEvents updateEvents={this.updateEvents}/>
         </Container>
       </div>
     );
   } 
-}
+};
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -2,10 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import * as atatus from 'atatus-js';
+// import * as atatus from 'atatus-js';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
-serviceWorker.register();
-atatus.config('5669942bf71a4ffea15ac4a5c6579835').install();
+if (process.env.NODE_ENV === 'production' && 'serviceWorker' in navigator) {
+  serviceWorker.register();
+}
+// atatus.config('5669942bf71a4ffea15ac4a5c6579835').install();
